15bf8dce42b8b1b6c79a9a8308013aba034b1fed
Andrii-Smolianinov <andr380v@gmail.com>
2022-08-27 19:55:48 +0300

    add preloader

---
+++ js/loader.js
@@ -0,0 +1,14 @@
+// window.addEventListener("load", () => {
+//   const loader = document.querySelector(".loader");
+
+//   loader.classList.add("loader--hiden");
+
+//   loader.addEventListener("transitioned", () => {
+//     document.body.removeChild("loader");
+//   });
+// });
+
+window.onload = function () {
+    let loader = document.getElementById('preloader');
+    loader.style.display = 'none'
+}
